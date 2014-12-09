1a8e76345ea27be59e709d551225709a5a8c13a9
Rob Nagle <rjn945@gmail.com>
2014-12-08 17:46:07 -0800

    New Lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to eco
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/eco/core_test.clj
@@ -0,0 +1,7 @@
+(ns eco.core-test
+  (:require [clojure.test :refer :all]
+            [eco.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
