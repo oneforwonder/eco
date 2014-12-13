7b4bc7492565f82c14b5d0e23b50db98157617e0
Rob Nagle <rjn945@gmail.com>
2014-12-12 20:16:45 -0800

    Simple ecology overview

---
+++ src/eco/ecology.clj
@@ -0,0 +1,60 @@
+(ns eco.ecology)
+
+(def resources [:light
+                :H20 :O2 :CO2 :NH3
+                :carbs :proteins :fats])
+
+(def organisms
+  {:plants
+    {:consumes [:light :H20 :NH3 :CO2]
+     :produces [:carbs]
+     :excretes [:O2]}
+
+   :herbivores
+    {:consumes [:carbs :O2 :H20]
+     :produces [:fats :proteins]
+     :excretes [:C02 :NH3]}
+
+  :carnivores
+    {:consumes [:proteins :fats :H20 :O2]
+     :produces []
+     :excretes [:C02 :NH3]}
+
+  :decomposers
+    {:consumes [:fats :proteins :carbs :02 :H20]
+     :produces []
+     :excretes [:C02 :NH3]}})
+
+
+;;;; -----------------------------------------------------
+
+(def organisms
+  {:contents []
+   :location [:x :y]
+   :state :hungry})
+
+(defn decompose-tile [tile]
+  (let [in (get-in organisms :decomposers :consumes)
+        out (get-in organisms :decomposers :excretes)]
+    ;TODO
+    ))
+(defn decompose-world [world]
+  (for [row world]
+    (for [tile world]
+      (decompose-tile tile))))
+
+(defn update-plant [organism world]
+    ;TODO
+  )
+
+(defn update-herbivore [organism world]
+    ;TODO
+  )
+
+(defn update-carnivore [organism world]
+    ;TODO
+  )
+
+(defn update-organism [organism world]
+    ;TODO
+  )
